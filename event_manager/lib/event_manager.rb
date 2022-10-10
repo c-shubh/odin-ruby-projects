9522cd163f7ff1ad4f6069e08ac699f60f4b3d7f
Shubh Chudasama <65857597+c-shubh@users.noreply.github.com>
2022-10-10 22:35:36 +0530

    Event manager initial commit

---
+++ event_manager/lib/event_manager.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'csv'
+require 'erb'
+require 'google-apis-civicinfo_v2'
+
+DEBUG = true
+
+require 'logger' if DEBUG
+$LOGGER = Logger.new($stdout) if DEBUG
+
+# Functions --------------------------------------------------------------------
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  begin
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+  $LOGGER.info { "Saved ./#{filename}" } if DEBUG
+end
+
+# Main -------------------------------------------------------------------------
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.rhtml')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+end
+
+$LOGGER.close if DEBUG
